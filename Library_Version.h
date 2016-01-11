91d0ed8636f310cb1e12a0a75590f0c44c7a4551
dparkermit <dparkermit@yahoo.com>
2016-01-11 09:36:43 -0800

    ETM LIBRARY Version 01
    This is the agile release for ETM LIBRARY v 01

--- Library_Version.h
+++ Library_Version.h
@@ -1,4 +1,4 @@
 #define ETM_CORE_VERSION              1_0_1
 #define ETM_EEPROM_EXTERNAL_VERSION   1_0_0
 #define ETM_EEPROM_INTERNAL_VERSION   1_0_0
-#define ETM_LINAC_CAN_VERSION         1_0_1
+#define ETM_LINAC_CAN_VERSION         0x17
